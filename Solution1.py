3e3c20793c2fa17ebf3f8e8dc4141814115acd99
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-07-12 08:26:21 +0530

    print upto 2 decimals in a float input

---
+++ Solution1.py
@@ -0,0 +1,21 @@
+### PROBLEM ###
+
+Float Input
+Requested files: InputOutput2.java (Download)
+Maximum number of files: 5
+Type of work: Individual work
+Write a program to get float as input and print it. When the user enters a float, it is stored in the variable test Float. Use '%.2f' in the print() to display float value with 2 precisions. NOTE: Bold letters or words are Input and the rest are output statements.
+
+Sample Input :
+
+56.2425
+
+Sample Output :
+
+56.24
+
+
+#### CODE FOR THE PROBLEM ######
+
+a=float(input())
+print(f"{a:.2f}")
