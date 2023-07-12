293b6cbcda522174342f0c06c724985d3f554f18
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-07-12 08:31:13 +0530

    print Ascii character of a value

---
+++ Solution3.py
@@ -0,0 +1,21 @@
+### PROBLEM ##
+
+Ascii-II
+Requested files: Ascii.java (Download)
+Maximum number of files: 5
+Type of work: Individual work
+ASCII - American Standard Code for Information Interchange. Write a program to get a number(ASCII) as input and print its equivalent character. You can display a character if you know ASCII code of that character. 
+
+Sample Input:
+
+103
+
+Sample Output:
+
+g
+
+  ### CODE FOR THE PROBLEM ###
+
+b=int(input())
+c=chr(b)
+print(c)
