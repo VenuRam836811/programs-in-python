c381c68dab587a5f1b912ec7a7af11e31937badd
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-07-12 21:21:12 +0530

    swap the given numbers

---
+++ Solution6.py
@@ -0,0 +1,43 @@
+### PROBLEM ###
+
+
+Swapping of two numbers
+Requested files: Swap.java (Download)
+Maximum number of files: 5
+Type of work: Individual work
+Write a program to get 2 numbers from the user and swap their values without any loss of data. You can make use of an additional 3rdvariable for swapping. Print the corresponding swapped values of the two numbers as output in the console. 
+
+Input format: 
+
+First input: an integer 
+
+Second input: an integer 
+
+Output format: 
+
+The output will be integers(swapped values)
+
+Sample Input:
+
+20
+
+10
+
+Sample Output:
+
+10
+
+20
+
+
+### CODE FOR THE PROBLEM ###
+
+
+a=int(input())
+b=int(input())
+c=a
+a=b
+b=c
+print(a)
+print(b)
+### 
