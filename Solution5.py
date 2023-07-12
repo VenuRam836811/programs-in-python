92bc02e8fcecbeed36c88cc9bce3c8b4a3e52668
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-07-12 21:14:48 +0530

    print product,quotient and remainder of given numbers

---
+++ Solution5.py
@@ -0,0 +1,54 @@
+### PROBLEM ###
+
+
+Product and Division
+Requested files: ProDiv.java (Download)
+Maximum number of files: 5
+Type of work: Individual work
+Write a program to get 2 numbers from the user and calculate their product, quotient and remainder using '*', '/' and '%' operators respectively. Print the corresponding product, quotient and remainder of the numbers as output in the console. 
+
+Input format: 
+
+First input: an integer
+
+Second input: an integer 
+
+Output format: 
+
+The first output will be the product of two integers 
+
+The second output will be the quotient 
+
+The third output will be the remainder
+
+Sample Input:
+
+50
+
+10
+
+Sample Output:
+
+500
+
+5
+
+0
+
+### CODE FOR THE PROBLEM ###
+
+
+def aaa(a,b):
+    print(a*b)
+def bbb(c):
+    print(c)
+def ccc(a,b):
+    print(a%b)
+a=int(input())
+b=int(input())
+c=int(a/b)
+aaa(a,b)
+bbb(c)
+ccc(a,b)
+
+
