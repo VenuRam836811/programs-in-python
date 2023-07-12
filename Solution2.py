ec77e03b1fff419fda915cae2f1aa5137b73c6c1
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-07-12 08:29:01 +0530

    print the Ascii value of a character

---
+++ Solution2.py
@@ -0,0 +1,21 @@
+### PROGRAM ###
+
+Ascii-I
+Requested files: InputOutput5.java (Download)
+Maximum number of files: 5
+Type of work: Individual work
+ASCII - American Standard Code for Information Interchange. Write a program to get a character as input and print it's ASCII value. When a character is entered, the character itself  stored.  And when we displayed that value using "int()"  type casting , the entered character is displayed as Ascii value. NOTE: Bold letters or words are Input and rest are output statements.
+
+Sample Input :
+
+g
+
+Sample Output:
+
+103
+
+### CODE FOR THE PROBLEM ###
+
+b=input()
+a=ord(b)
+print(a)
