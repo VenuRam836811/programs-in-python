b6bb9f8da18bd81d4ff12a434d60423e017be44f
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-07-17 19:05:10 +0530

    Write a Python Program to solve the pattern in dynamic.

---
+++ Solution70.py
@@ -0,0 +1,32 @@
+### PROBLEM ###
+
+Pattern I
+Requested files: Pattern1.java (Download)
+Maximum number of files: 5
+Type of work: Individual work
+Write a Python Program to solve the pattern in dynamic.
+
+Sample Input 1:
+5
+Sample Output 1:
+*****
+*****
+*****
+*****
+*****
+Sample Input 2:
+7
+Sample Output 2:
+*******
+*******
+*******
+*******
+*******
+*******
+*******
+
+### CODE FOR THE PROBLEM ###
+
+a=int(input())
+for i in range(a):
+    print("*"*a)
