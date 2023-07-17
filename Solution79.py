2c7fa105a345bf12010a0aea7168ba70d18a1d2c
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-07-17 20:00:57 +0530

    Write a Python program to print the given pattern from user-defined values(dynamic values).

---
+++ Solution79.py
@@ -0,0 +1,46 @@
+### PROBLEM ###
+
+Pattern X
+Requested files: Pattern10.java (Download)
+Maximum number of files: 5
+Type of work: Individual work
+Write a Python program to print the given pattern from user-defined values(dynamic values).
+
+Sample input 1:
+5
+Sample output 1:
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    * 
+Sample input 2:
+8
+Sample output 2:
+       *
+      ***
+     *****
+    *******
+   *********
+  ***********
+ *************
+***************
+ *************
+  ***********
+   *********
+    *******
+     *****
+      ***
+       *
+
+### CODE FOR THE PROBLEM ###
+
+a=int(input())
+for i in range(1,a+1):
+    print(" "*(a-i)+"*"*i+"*"*(i-1))
+for i in range(1,a):
+    print(" "*i+"*"*(a-i)+"*"*(a-1-i))
