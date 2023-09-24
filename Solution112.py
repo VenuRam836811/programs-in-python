25dffb21602d9e3659ced5744aec6b5e99158da2
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-09-24 14:38:27 +0530

    The absolute difference between A^B and X^Y rounded to the closest integer Constraints

---
+++ Solution112.py
@@ -0,0 +1,32 @@
+### PROBLEM ###
+
+
+Story:
+You were out partying with your friends and as the designated driver you decided to stay sober. Unfortunately the cop that pulled you over would disagree with you. For some reason the only way that the cop can prove your sobriety is by completing a maths challenge.
+The Challenge
+Output the absolute difference between two powers A^B and X^Y, rounded to the closest integer.
+Input
+Line 1 Two positive floats A and X.
+Line 2 Two positive integers B and Y that will be exponents
+Output
+Line 1 The absolute difference between A^B and X^Y rounded to the closest integer
+Constraints
+A,X no more than 2 decimal places
+Example
+Input
+4 2
+3 5
+Output
+32
+
+
+
+### CODE FOR THE PROBLEM ###
+
+
+
+import sys
+import math
+a,x=[float(i) for i in input().split()]
+b,y=[int(i) for i in input().split()]
+print(abs(int(math.pow(a,b)-math.pow(x,y))))
